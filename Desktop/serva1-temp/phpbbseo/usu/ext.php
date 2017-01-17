5228b11faf32d29dea6a6ffc8a75e9caaf9d4a91
Jamie Seignior <C:\Users\4X\AppData\Roaming\The Bat!>
2017-01-17 22:48:18 +1100

    First commit

---
+++ Desktop/serva1-temp/phpbbseo/usu/ext.php
@@ -0,0 +1,20 @@
+<?php
+/**
+*
+* @package Ultimate SEO URL phpBB SEO
+* @version $$
+* @copyright (c) 2014 www.phpbb-seo.com
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbseo\usu;
+
+/**
+* ext Class
+* www.phpBB-SEO.com
+* @package Ultimate SEO URL phpBB SEO
+*/
+class ext extends \phpbb\extension\base
+{
+}
